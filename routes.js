8347554bb1bf4ba55f97d124c5af14db934c11e5
VinayakPatkar <vinayakpatkar017@gmail.com>
2023-02-04 20:05:24 +0530

    third commit

--- routes.js
+++ routes.js
@@ -1,10 +1,53 @@
-
-
-
-
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'pages','HTML','signin.html'))
-})
-app.listen(process.env.PORT || 8000, () => {
-    console.log('listening on port '+ (process.env.PORT || 8000));
- })
+const path = require('path')
+const mongoose = require('mongoose')
+function routes(app,dbe,lms,accounts)
+{
+    app.get('/',(req,res)=>{
+        res.status(200).sendFile(path.join(__dirname,'pages','HTML','signin.html'))
+    })
+    app.post('/signin',(req,res)=>{
+        const {Email,Password} = req.body;
+        db.findOne({Email},async(err,pres)=>{
+            if(pres)
+            {
+                console.log('Welcome')
+                res.status(200).sendFile(path.join(__dirname,'pages','HTML','Dashboard.html'))
+            }
+            else
+            {
+                console.log('not present try to login')
+                res.status(400).sendFile(path.join(__dirname,'pages','HTML','error.html'))
+            }
+        })
+    })
+    app.get('/signup',(req,res)=>{
+        res.status(200).sendFile(path.join(__dirname,'pages','HTML','signup.html'))
+    })
+    app.post('/signup',(req,res)=>{
+        const {BSname,BSAdd,BSType,BSSize,Email,Pass,ConfPass} = req.body;
+        db.findOne({Email},async(err,pres)=>{
+            if(pres)
+            {
+                console.log('Already Present')
+                res.status(400).sendFile(path.join(__dirname,'pages','HTML','error.html'))
+            }
+            else
+            {
+                const proc = await db.insertOne({BSname,BSAdd,BSType,BSSize,Email,Pass,ConfPass})
+                    if(proc){
+                        console.log('Stored in DB');
+                        dataBase = true
+                    }else{
+                        console.log('Some error occured');
+                    }
+                res.status(200).sendFile(path.join(__dirname,'pages','HTML','error.html'))
+            }
+        })
+    })
+    app.get('/userForm',(req,res)=>{
+        res.status(200).sendFile(path.join(__dirname,'pages','HTML','user_form.html'))
+    })
+    app.post('/userForm',(req,res)=>{
+        
+    })
+}
--- server.js
+++ server.js
@@ -1,54 +1,30 @@
 const express = require('express');
 const path = require('path');
 const mongodb = require('mongodb').MongoClient
+const contract = require('truffle-contract');
+const artifacts = require('./build/Documents.json');
 const mongoose = require('mongoose');
 const app = express()
-mongoose.connect("mongodb://localhost:27017/test",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-},console.log('Database connected'))
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'pages','HTML','signin.html'))
+if (typeof web3 !== 'undefined') {
+    var web3 = new Web3(web3.currentProvider)
+  } else {
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+}
+
+const LMS = contract(artifacts)
+LMS.setProvider(web3.currentProvider)
+
+mongodb.connect('mongodb://localhost:27017/test',{ useUnifiedTopology: true }, async(err,client)=>{
+    if(client){
+        console.log('Done')
+    }
+    await client.connect()
+    const db =client.db('Doc')
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0])
+    const lms = await LMS.deployed();
+    routes(app,db, lms, accounts)
+    app.listen(process.env.PORT || 3005, () => {
+        console.log('listening on port '+ (process.env.PORT || 3005));
+     })
 })
-app.post('/signin',(req,res)=>{
-    const {Email,Password} = req.body;
-    db.findOne({Email},async(err,pres)=>{
-        if(pres)
-        {
-            console.log('Welcome')
-            res.status(200).sendFile(path.join(__dirname,'pages','HTML','Dashboard.html'))
-        }
-        else
-        {
-            console.log('not present try to login')
-            res.status(400).sendFile(path.join(__dirname,'pages','HTML','error.html'))
-        }
-    })
-})
-app.get('/signup',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'pages','HTML','signup.html'))
-})
-app.post('/signup',(req,res)=>{
-    const {BSname,BSAdd,BSType,BSSize,Email,Pass,ConfPass} = req.body;
-    db.findOne({Email},async(err,pres)=>{
-        if(pres)
-        {
-            console.log('Already Present')
-            res.status(400).sendFile(path.join(__dirname,'pages','HTML','error.html'))
-        }
-        else
-        {
-            const proc = await db.insertOne({BSname,BSAdd,BSType,BSSize,Email,Pass,ConfPass})
-                if(proc){
-                    console.log('Stored in DB');
-                    dataBase = true
-                }else{
-                    console.log('Some error occured');
-                }
-            res.status(200).sendFile(path.join(__dirname,'pages','HTML','error.html'))
-        }
-    })
-})
-app.listen(process.env.PORT || 3005, () => {
-    console.log('listening on port '+ (process.env.PORT || 3005));
- })
